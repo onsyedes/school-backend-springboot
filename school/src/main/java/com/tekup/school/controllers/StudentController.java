08ecfcfa12b0af98a3e2b966d0f9676037567609
onsyedes <ons.yedes@tek-up.de>
2021-10-05 17:32:18 -0700

    Added delete,post and search by firstname methods to Student Controller, Added findByFirstnameContains(firstname) to SrudentRepository

---
+++ school/src/main/java/com/tekup/school/controllers/StudentController.java
@@ -0,0 +1,45 @@
+package com.tekup.school.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tekup.school.entities.Student;
+import com.tekup.school.repository.StudentRepository;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+@Autowired
+StudentRepository studentRepository;
+
+@PostMapping("/add")
+public Student createStudent(@RequestBody Student student) {
+	return studentRepository.save(student);
+}
+@DeleteMapping("/delete/{id}")
+public ResponseEntity<Long> Student(@PathVariable(value="id") Long id) throws ResourceNotFoundException{
+	Student student= studentRepository.findById(id).orElseThrow(
+			()-> new ResourceNotFoundException("Student not found for this id "+id));
+	studentRepository.delete(student);
+	return new ResponseEntity<>(id, HttpStatus.OK);
+	
+}
+@GetMapping("/search/{firstname}")
+public List<Student> findStudentByFirstname(@PathVariable(value="firstname") String firstname){
+	return studentRepository.findByFirstNameContains(firstname);
+}
+
+	
+}
--- school/src/main/java/com/tekup/school/repository/StudentRepository.java
+++ school/src/main/java/com/tekup/school/repository/StudentRepository.java
@@ -1,10 +1,14 @@
 package com.tekup.school.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tekup.school.entities.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	List<Student> findByFirstNameContains(String firstname);
 
 }
