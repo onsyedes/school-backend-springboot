ca7800c38297487d704ce9f8514333e0d07ec348
onsyedes <ons.yedes@tek-up.de>
2021-11-10 16:41:55 -0800

    layouts

--- school/src/main/java/com/tekup/school/SchoolApplication.java
+++ school/src/main/java/com/tekup/school/SchoolApplication.java
@@ -2,8 +2,8 @@ package com.tekup.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+
 public class SchoolApplication {
 
 	public static void main(String[] args) { 
