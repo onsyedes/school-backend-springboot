4c7340ae35dcc8e1505e245528a26fa32b8909c3
onsyedes <ons.yedes@tek-up.de>
2021-11-10 16:53:59 -0800

    ccc
    cccc

--- school/src/main/java/com/tekup/school/controllers/StudentController.java
+++ school/src/main/java/com/tekup/school/controllers/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tekup.school.entities.Classe;
 import com.tekup.school.entities.Student;
@@ -20,13 +22,24 @@ import com.tekup.school.repository.ClasseRepository;
 import com.tekup.school.repository.StudentRepository;
 
 @RestController
-@RequestMapping("/students")
+//@RequestMapping("/students")
 public class StudentController {
 
 @Autowired
 StudentRepository studentRepository;
 @Autowired
 ClasseRepository classeRepository;
+
+
+/*
+@GetMapping("/students_list")
+public ModelAndView showStudents(Model model) {
+	
+	 
+}
+*/
+
+
 
 @PostMapping("/add")
 public Student createStudent(@RequestBody Student student) {
